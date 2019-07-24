c440869c6b11910b5f2ced1bd4ef31e0904781f8
Adelin Owona <adelinowona@gmail.com>
2019-07-24 16:40:49 -0700

    Added starter code into files

---
+++ main.py
@@ -0,0 +1,25 @@
+#main.py
+# the import section
+import webapp2
+import os
+import jinja2
+import json
+
+
+from google.appengine.api import urlfetch
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('home.html')
+
+        self.response.write(template.render(a_variable_dict))
+
+# the app configuration section
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
