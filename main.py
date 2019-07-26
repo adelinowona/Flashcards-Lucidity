4ac9e6b3cc1517d527de4d6d63413547689ab8e3
Han Pham <hanpm36@gmail.com>
2019-07-25 18:40:45 -0700

    added form, difficulty levels, and buttons

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ import webapp2
 import os
 import jinja2
 import json
-
-
-
-from google.appengine.api import urlfetch
+from models import Question
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -19,19 +16,42 @@ class MainPage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('templates/home.html')
         self.response.write("Welcome")
 
-class SignUp(webapp2.RequestHandler):
-    def get(self):
-        template = JINJA_ENVIRONMENT.get_template('templates/home.html')
-        self.response.write("Welcome")
+# class SignUp(webapp2.RequestHandler):
+#     def get(self):
+#         template = JINJA_ENVIRONMENT.get_template('templates/home.html')
+#         self.response.write("Welcome")
+#
+# class Profile(webapp2.RequestHandler):
+#     def get(self):
+#         template = JINJA_ENVIRONMENT.get_template('templates/home.html')
+#         self.response.write("Welcome")
 
-class Profile(webapp2.RequestHandler):
+class Create(webapp2.RequestHandler):
     def get(self):
-        template = JINJA_ENVIRONMENT.get_template('templates/home.html')
-        self.response.write("Welcome")
+        create_template = JINJA_ENVIRONMENT.get_template('templates/create.html')
+        self.response.write(create_template.render())
+
+    def post(self):
+        the_question = self.request.get('question')
+        the_answer = self.request.get('answer')
+
+        card = Question(question=the_question, answer= the_answer, level="easy")
+        card_two = Question(question=the_question, answer= the_answer, level="medium")
+        card_three = Question(question=the_question, answer= the_answer, level="hard")
+        # card_data_answers = Hard(multiple_answers = the_answer)
+        card.put()
+        self.redirect('/')
+
+
+
+
+
 
 # the app configuration section
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/signup', SignUp),
-    ('/profile', Profile)
+    # ('/signup', SignUp),
+    # ('/profile', Profile)
+    ('/create', Create),
+
 ], debug=True)
--- models.py
+++ models.py
@@ -1,23 +1,25 @@
-from google.appengine.api import ndb
+from google.appengine.ext import ndb
 
-class Easy(ndb.Model):
-    content = ndb.StringProperty(required=True)
-    user = ndb.KeyProperty(User)
+class Question(ndb.Model):
+    level = ndb.StringProperty(required=True)
+    question = ndb.StringProperty(required=True)
+    answer = ndb.StringProperty(required=True)
+    # user = ndb.KeyProperty(required=True)
 
-class Medium(ndb.Model):
-    content = ndb.StringProperty(required=True)
-    user = ndb.KeyProperty(User)
-
-class Hard(ndb.Model):
-    content = ndb.StringProperty(required=True)
-    user = ndb.KeyProperty(User)
-
-class User(ndb.Model):
-    user_name = ndb.StringProperty(required=True)
-    user_id = ndb.StringProperty()
-
-class Flashcard_set(ndb.Model):
-    easy_cards = ndb.KeyProperty(Easy)
-    medium_cards = ndb.KeyProperty(Medium)
-    hard_cards = ndb.KeyProperty(Hard)
-    owner = ndb.KeyProperty(User)
+# class Medium(ndb.Model):
+#     content = ndb.StringProperty(required=True)
+#     user = ndb.KeyProperty(User)
+#
+# class Hard(ndb.Model):
+#     content = ndb.StringProperty(required=True)
+#     user = ndb.KeyProperty(User)
+#
+# class User(ndb.Model):
+#     user_name = ndb.StringProperty(required=True)
+#     user_id = ndb.StringProperty()
+#
+# class Flashcard_set(ndb.Model):
+#     easy_cards = ndb.KeyProperty(Easy)
+#     medium_cards = ndb.KeyProperty(Medium)
+#     hard_cards = ndb.KeyProperty(Hard)
+#     owner = ndb.KeyProperty(User)
