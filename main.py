4c85f2d5a96147aa64ba25ef1f2db95796749320
Han Pham <hanpm36@gmail.com>
2019-07-26 12:05:50 -0700

    add different levels, and imported random

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-#main.py
+# main.py
 # the import section
 import webapp2
 import os
 import jinja2
 import json
-from models import Question
+from models import Card
+import random
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -21,10 +22,10 @@ class MainPage(webapp2.RequestHandler):
 #         template = JINJA_ENVIRONMENT.get_template('templates/home.html')
 #         self.response.write("Welcome")
 #
-# class Profile(webapp2.RequestHandler):
-#     def get(self):
-#         template = JINJA_ENVIRONMENT.get_template('templates/home.html')
-#         self.response.write("Welcome")
+class Profile(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/home.html')
+        self.response.write("Hey there!")
 
 class Create(webapp2.RequestHandler):
     def get(self):
@@ -34,13 +35,27 @@ class Create(webapp2.RequestHandler):
     def post(self):
         the_question = self.request.get('question')
         the_answer = self.request.get('answer')
+        difficulty = int(self.request.get('difficulty_selection'))
 
-        card = Question(question=the_question, answer= the_answer, level="easy")
-        card_two = Question(question=the_question, answer= the_answer, level="medium")
-        card_three = Question(question=the_question, answer= the_answer, level="hard")
+        card = Card(question=the_question, answer= the_answer, level=difficulty)
+
         # card_data_answers = Hard(multiple_answers = the_answer)
         card.put()
-        self.redirect('/')
+        self.redirect('/create')
+
+
+class Sort(webapp2.RequestHandler):
+    def get(self):
+        # some_levels = Card.query().order(Card.level).fetch()
+        # sort_template = JINJA_ENVIRONMENT.get_template('templates/home.html')
+        # self.response.write(sort_template.render({"level_info": some_levels}))
+        template = JINJA_ENVIRONMENT.get_template('templates/flashcard.html')
+        card = Card.query().filter(Card.level == random.randint(1,3)).get()
+        dict_for_template = {
+            "my_answer": card.answer,
+            "my_question": card.question,
+        }
+        self.response.write(template.render(dict_for_template))
 
 
 
@@ -51,7 +66,8 @@ class Create(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     # ('/signup', SignUp),
-    # ('/profile', Profile)
+    ('/profile', Profile),
     ('/create', Create),
+    ('/sort', Sort)
 
 ], debug=True)
--- models.py
+++ models.py
@@ -1,25 +1,7 @@
 from google.appengine.ext import ndb
 
-class Question(ndb.Model):
-    level = ndb.StringProperty(required=True)
+class Card(ndb.Model):
+    level = ndb.IntegerProperty(required=True)
     question = ndb.StringProperty(required=True)
     answer = ndb.StringProperty(required=True)
     # user = ndb.KeyProperty(required=True)
-
-# class Medium(ndb.Model):
-#     content = ndb.StringProperty(required=True)
-#     user = ndb.KeyProperty(User)
-#
-# class Hard(ndb.Model):
-#     content = ndb.StringProperty(required=True)
-#     user = ndb.KeyProperty(User)
-#
-# class User(ndb.Model):
-#     user_name = ndb.StringProperty(required=True)
-#     user_id = ndb.StringProperty()
-#
-# class Flashcard_set(ndb.Model):
-#     easy_cards = ndb.KeyProperty(Easy)
-#     medium_cards = ndb.KeyProperty(Medium)
-#     hard_cards = ndb.KeyProperty(Hard)
-#     owner = ndb.KeyProperty(User)
