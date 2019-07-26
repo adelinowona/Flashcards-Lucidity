0add5cb3991f536ec9931477d616f967cad6f18d
Adelin Owona <adelinowona@gmail.com>
2019-07-26 12:18:56 -0700

    Did the backend stuff

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import webapp2
 import os
 import jinja2
 import json
-
-
+from models import Profile
+from google.appengine.api import users
 from google.appengine.api import urlfetch
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -13,29 +13,79 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+
+
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
-<<<<<<< HEAD
-        main_template = JINJA_ENVIRONMENT.get_template('templates/home.html')
-        self.response.write("test")
+        user = users.get_current_user()
+        # If the user is logged in...
+        if user:
+          email_address = user.nickname()
+          current_user = Profile.query().filter(Profile.email == email_address).get()
+          # If the user is registered...
+          if current_user:
+            # Greet them with their personal information
+            self.redirect("/profile0")
+          # If the user isn't registered...
+          else:
+            # Offer a registration form for a first-time visitor:
+            self.redirect('/profile')
 
-=======
-        template = JINJA_ENVIRONMENT.get_template('templates/home.html')
+        else:
+          # If the user isn't logged in...
+          login_url = users.create_login_url('/')
+          template = JINJA_ENVIRONMENT.get_template('templates/Googlelogin.html')
+          dict_variable = {
+            "login_url": login_url
+          }
+          self.response.write(template.render(dict_variable))
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("You are signed out")
+
+
+
+class ProfileHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/profile.html')
         self.response.write(template.render())
 
-class SignUp(webapp2.RequestHandler):
-    def get(self):
-        template = JINJA_ENVIRONMENT.get_template('templates/home.html')
-        self.response.write("Welcome")
 
-class Profile(webapp2.RequestHandler):
+
+
+    def post(self):
+        user_name = self.request.get('username')
+        name = self.request.get('name')
+        email = self.request.get('email')
+        current_user = Profile(user_name=user_name, name=name, email=email,)
+        current_user.put()
+        dict_variable = {
+            "username": user_name
+        }
+        template = JINJA_ENVIRONMENT.get_template('templates/profile0.html')
+        self.response.write(template.render(dict_variable))
+
+class Profile0Handler(webapp2.RequestHandler):
     def get(self):
-        template = JINJA_ENVIRONMENT.get_template('templates/home.html')
-        self.response.write("Welcome")
->>>>>>> a2fddf538fc8ad030562374f4e0a458a46b0edb9
+        user = users.get_current_user()
+        email_address = user.nickname()
+        signout_link_html = users.create_logout_url('/login')
+        current_user = Profile.query().filter(Profile.email == email_address).get()
+        if current_user:
+            dict_variable = {
+                "username": current_user.user_name,
+                "login_url": signout_link_html
+            }
+            template = JINJA_ENVIRONMENT.get_template('templates/profile0.html')
+            self.response.write(template.render(dict_variable))
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    # ('/signup', SignUp),
-    # ('/profile', Profile)
+    ('/login', LoginHandler),
+    ('/profile', ProfileHandler),
+    ('/profile0', Profile0Handler)
+
 ], debug=True)
--- models.py
+++ models.py
@@ -1,23 +1,25 @@
-from google.appengine.api import ndb
+from google.appengine.ext import ndb
 
-class Easy(ndb.Model):
-    content = ndb.StringProperty(required=True)
-    user = ndb.KeyProperty(User)
+# class Easy(ndb.Model):
+#     content = ndb.StringProperty(required=True)
+#     user = ndb.KeyProperty(Profile)
+#
+# class Medium(ndb.Model):
+#     content = ndb.StringProperty(required=True)
+#     user = ndb.KeyProperty(Profile)
+#
+# class Hard(ndb.Model):
+#     content = ndb.StringProperty(required=True)
+#     user = ndb.KeyProperty(Profile)
 
-class Medium(ndb.Model):
-    content = ndb.StringProperty(required=True)
-    user = ndb.KeyProperty(User)
-
-class Hard(ndb.Model):
-    content = ndb.StringProperty(required=True)
-    user = ndb.KeyProperty(User)
-
-class User(ndb.Model):
+class Profile(ndb.Model):
     user_name = ndb.StringProperty(required=True)
+    name = ndb.StringProperty()
+    email = ndb.StringProperty()
     user_id = ndb.StringProperty()
 
-class Flashcard_set(ndb.Model):
-    easy_cards = ndb.KeyProperty(Easy)
-    medium_cards = ndb.KeyProperty(Medium)
-    hard_cards = ndb.KeyProperty(Hard)
-    owner = ndb.KeyProperty(User)
+# class Flashcard_set(ndb.Model):
+#     easy_cards = ndb.KeyProperty(Easy)
+#     medium_cards = ndb.KeyProperty(Medium)
+#     hard_cards = ndb.KeyProperty(Hard)
+#     owner = ndb.KeyProperty(User)
